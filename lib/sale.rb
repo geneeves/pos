7e213ce8284d786ef47fc277b812adacef1eb82c
Corbin Dahl & Geneva Skeen <student@epicodus.com>
2013-09-20 17:57:02 -0700

    update product model

--- lib/sale.rb
+++ lib/sale.rb
@@ -1,4 +1,13 @@
+require 'pry'
+
 class Sale <ActiveRecord::Base
+  belongs_to :cashier
   has_many :purchases
   has_many :products, through: :purchases 
+
+  def total
+    self.purchases.each do |product|
+      puts (purchase.product.price * purchase.quantity)
+    end
+  end
 end

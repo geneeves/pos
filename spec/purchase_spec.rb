e532e04b52c19ceceee735888618c92b59016407
geneeves <geneeves@users.noreply.github.com>
2013-11-07 18:01:48 -0800

    Update purchase_spec.rb

--- spec/purchase_spec.rb
+++ spec/purchase_spec.rb
@@ -3,8 +3,4 @@ require 'spec_helper'
 describe Purchase do
   it { should belong_to(:product) }
   it { should belong_to(:sale) }
-
-  it "gives you the total amount of each product purchased" do
-
-  
 end
